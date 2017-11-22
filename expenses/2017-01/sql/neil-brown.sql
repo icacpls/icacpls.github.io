0f1e77d0282c306d750e70eb374eb5b97462cf10
icacpls <icacpls@gmail.com>
2017-11-22 12:17:12 +1100

    Added missing details & mugs

---
+++ expenses/2017-01/sql/neil-brown.sql
@@ -0,0 +1,10 @@
+INSERT INTO Expense (Name, MemberType, Electorate, State, Party, Category, SubCategory, Period, Details, Amount, Notes, Date)
+VALUES ('Neil Brown', 'Representative', 'Menzies', 'Victoria', 'Liberal Party of Australia', 'Domestic Scheduled Fares', 'Fares', 'From 1 July 2016', '25 Nov 16 - Melbourne to Sydney', 814.95, '', '01-Jul-2016');
+INSERT INTO Expense (Name, MemberType, Electorate, State, Party, Category, SubCategory, Period, Details, Amount, Notes, Date)
+VALUES ('Neil Brown', 'Representative', 'Menzies', 'Victoria', 'Liberal Party of Australia', 'Domestic Scheduled Fares', 'Fares', 'From 1 July 2016', '25 Nov 16 - Sydney to Melbourne', 814.95, '', '01-Jul-2016');
+INSERT INTO Expense (Name, MemberType, Electorate, State, Party, Category, SubCategory, Period, Details, Amount, Notes, Date)
+VALUES ('Neil Brown', 'Representative', 'Menzies', 'Victoria', 'Liberal Party of Australia', 'Domestic Scheduled Fares', 'Fares', 'From 1 July 2016', '29 Nov 16 - Melbourne to Sydney', 814.95, '', '01-Jul-2016');
+INSERT INTO Expense (Name, MemberType, Electorate, State, Party, Category, SubCategory, Period, Details, Amount, Notes, Date)
+VALUES ('Neil Brown', 'Representative', 'Menzies', 'Victoria', 'Liberal Party of Australia', 'Domestic Scheduled Fares', 'Fares', 'Before 1 July 2016', '23 Jun 16 - Melbourne to Sydney', 780.82, '', '01-Jul-2016');
+INSERT INTO Expense (Name, MemberType, Electorate, State, Party, Category, SubCategory, Period, Details, Amount, Notes, Date)
+VALUES ('Neil Brown', 'Representative', 'Menzies', 'Victoria', 'Liberal Party of Australia', 'Domestic Scheduled Fares', 'Fares', 'Before 1 July 2016', '25 Jun 16 - Sydney to Melbourne', 780.82, '', '01-Jul-2016');
